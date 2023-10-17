ba5ee10769f3ee5be68f9e0e31b709319a1d0aa5
JoaoFFreitas <jmdsff@gmail.com>
2023-10-17 07:48:09 +0100

    1st

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="games for sale for ps5">
+    <meta name="author" content="João Freitas">
+    <title>GameShop</title>
+</head>
+<h1>ola</h1>
+
+<body>
+
+</body>
+
+</html>
